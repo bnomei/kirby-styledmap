bd9497551fcae85b44e2bc14df5b63f8bf8e322d
Bruno Meilick <b@bnomei.com>
2017-05-22 19:12:39 +0200

    fixed widget license link
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- widgets/styledmap/styledmap.php
+++ widgets/styledmap/styledmap.php
@@ -8,7 +8,7 @@ return [
   ],
   'html' => function() {
     return tpl::load(__DIR__ . DS . 'styledmap.html.php', array(
-      'text' => '<b>Kirby Styledmap</b> is running in trial mode. Please support the development of this plugin and <a href="https://sites.fastspring.com/bnomei/product/kirby-styledmap" target="_blank">buy a license</a>. If you already have a license key, please add it to your <code title="site/config/config.php" style="border-bottom: 1px dotted; font-family: monospace;">config.php</code> file.',
+      'text' => '<b>Kirby Styledmap</b> is running in trial mode. Please support the development of this plugin and <a href="https://bnomei.onfastspring.com/kirby-styledmap" target="_blank">buy a license</a>. If you already have a license key, please add it to your <code title="site/config/config.php" style="border-bottom: 1px dotted; font-family: monospace;">config.php</code> file.',
     ));
   }
 ];
